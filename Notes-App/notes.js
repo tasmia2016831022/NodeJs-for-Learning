30bb6322325cb27e42a8057de3c8ea3ba39de4e2
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-03 18:46:01 +0600

    duplicate note detect

--- Notes-App/notes.js
+++ Notes-App/notes.js
@@ -5,11 +5,23 @@ const getNotes = function(){
 
 const addNote = function(title, body){
     const notes = loadNotes();
-    notes.push({
-        title: title,
-        body: body
+    const duplicateNotes = notes.filter(function(note){
+        console.log(note.title === title);
+        return note.title === title;
     })
-    saveNotes(notes);
+    
+    if(duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes);
+        console.log("New note added");
+    }else{
+        console.log("Note title taken");
+    }
+    console.log(duplicateNotes);
+
 }
 
 const saveNotes = function(notes){
