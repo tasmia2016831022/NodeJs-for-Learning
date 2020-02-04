5f6693563415c396379c07d1c3edce0ef311ab49
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-04 18:24:01 +0600

    challenge01 , section 6 done

---
+++ Section-06-AsynchronousProgramming/challenge01.md
@@ -0,0 +1,8 @@
+# print a small forcast to the user
+
+1. Print: "It's currently 58.55 degrees out There's a 0% chance of rain.
+
+2. Test your work.
+
+* Commit:
+* Link: 
--- Weather-App/app.js
+++ Weather-App/app.js
@@ -1,10 +1,13 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/be80abcef967d716159ce841ccb5516f/23.7937,90.4107';
+const url = 'https://api.darksky.net/forecast/be80abcef967d716159ce841ccb5516f/63.0937,90.4107';
 
 request({
     url: url,
     json: true
 } , (error, response) => {
-    console.log(response.body.timezone)
+    const temperature = response.body.currently.temperature;
+    const percentage = (response.body.currently.precipProbability) * 100;
+    const precipType = response.body.currently.precipType;
+    console.log(`This is ${temperature} degree now. there's a ${percentage} % chance of ${precipType}ing`)
 })
