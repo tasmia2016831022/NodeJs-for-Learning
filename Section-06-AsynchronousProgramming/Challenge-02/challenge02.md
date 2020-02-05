af35e4fac5f0617d44980a48ff281da416a4a668
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-05 12:56:33 +0600

    challenge 02 , section 06 done

---
+++ Section-06-AsynchronousProgramming/Challenge-02/challenge02.md
@@ -0,0 +1,10 @@
+# Print the latitude and longitude of Dhaka
+
+1. Fire off a new request to the URL explored in browser.
+
+2. Have the request module parsed in json.
+3. Print both latitude and longitude in the terminal.
+4. Test your work.
+
+* Commit:
+* Link: 
--- Weather-App/app.js
+++ Weather-App/app.js
@@ -1,13 +1,40 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/be80abcef967d716159ce841ccb5516f/63.0937,90.4107';
+const url = 'https://api.darksky.net/forecast/be80abcef967d716159ce841ccb5516f/23.7,90.36667?lang=bn';
 
 request({
     url: url,
     json: true
 } , (error, response) => {
-    const temperature = response.body.currently.temperature;
-    const percentage = (response.body.currently.precipProbability) * 100;
-    const precipType = response.body.currently.precipType;
-    console.log(`This is ${temperature} degree now. there's a ${percentage} % chance of ${precipType}ing`)
+    if(error){
+        console.log(error);
+    }else{
+        const temperature = response.body.currently.temperature;
+        const percentage = (response.body.currently.precipProbability) * 100;
+        const precipType = response.body.currently.precipType;
+        const summary = response.body.daily.data[0].summary;
+        console.log(`${summary} This is ${temperature} degree now. there's a ${percentage} % chance of ${precipType}ing`)
+    }
 })
+
+const mapboxAT = `pk.eyJ1IjoidGFzbWlhIiwiYSI6ImNrNjd3MWVrdzBnY2Qza284cjI3bmFpMGsifQ.zrWV1-nUyqizb5yZc2YWBQ`;
+const endpoint = `Dhaka`;
+const limit = 1;
+const mapurl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${endpoint}.json?access_token=${mapboxAT}&limit=${limit}`;
+
+request({
+    url: mapurl,
+    json: true
+},(error, response) =>{
+    if(error){
+        console.log(error);
+    }else{
+        const location = response.body.query.join(' ');
+        const lat = response.body.features[0].center[0];
+        const long = response.body.features[0].center[1];
+        
+        console.log(`${location} is at latitude: ${lat} and longitude: ${long}`);
+    }
+})
+
+
