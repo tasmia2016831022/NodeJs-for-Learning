a7ae59fd4b3f2e57cc74a8b4828967cc8a7fa161
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-06 16:27:40 +0600

    style added and rendered

--- Section-07-WebServer/src/app.js
+++ Section-07-WebServer/src/app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const app = express();
 const publicDirectoryPath = path.join(__dirname,'../public');
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath));
 
 // app.get('', (req, res) => {
