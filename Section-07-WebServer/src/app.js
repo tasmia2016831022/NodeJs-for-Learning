56215b411104006ad293a217d602207d562be02a
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-09 18:48:06 +0600

    error unsolved , occured in forecast

--- Section-07-WebServer/src/app.js
+++ Section-07-WebServer/src/app.js
@@ -48,7 +48,7 @@ app.get('/weather',(req,res) => {
             error: 'You must provide an address'
         })
     }
-    geocode(req.query.address,(error,{latitude,longitude,location})=>{
+    geocode(req.query.address,(error,{latitude,longitude,location} = {})=>{
         if(error){
             return res.send(error)
         }else{
