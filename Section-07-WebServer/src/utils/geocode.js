91cf6165555ca99a8856c8b3aa8af2df4c8041f9
tasmia2016831022 <tasmia22@student.sust.edu>
2020-02-09 17:47:21 +0600

    updated folder

---
+++ Section-07-WebServer/src/utils/geocode.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const token = require('./tokens.js');
+
+const geocode = (endpoint, callback) => {
+    const limit = 1;
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${endpoint}.json?access_token=${token.MapBoxAT}&limit=${limit}`;
+    
+    request({
+        url,
+        json: true
+    }, (error,{body}) => {
+        if(error){
+            callback(error,undefined);
+        }else if(body.message){
+           callback(body.message, undefined);
+        }else{
+            callback(undefined,{
+                latitude: body.features[0].center[0],
+                longitude: body.features[0].center[1],
+                location: body.features[0].place_name
+            })
+        }
+    })
+   
+}
+
+module.exports = geocode;
